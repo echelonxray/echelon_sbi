22b014100a201ded22e1babcacd6db6aaf775732
Michael T. Kloos <michael@michaelkloos.com>
2021-10-03 17:50:54 -0400

    Add stack pointer symbol and location control through linking and pc relative assembly

--- src/kernel/kstart_entry.s
+++ src/kernel/kstart_entry.s
@@ -13,25 +13,22 @@ my_entry_pt:
   mv s2, a3
   call mul
   add a5, s3, a4
-  sw a0, 0(a5)
-  sw a1, 8(a5)
-  sw a2, 16(a5)
-  sw a3, 24(a5)
+  #sw a0, 0(a5)
+  #sw a1, 8(a5)
+  #sw a2, 16(a5)
+  #sw a3, 24(a5)
   
   li a2, 1
-  bne a3, a2, loop
+  bne a3, a2, clear_and_loop
   
-  #csrw satp, t6
-  #csrr t6, satp
-  #csrr t6, mstatus
+  # Load the location of symbol KISTACK_TOP into Stack Pointer
+  # This is done using pc relative addressing so that it works
+  # across 32-bit, 64-bit, and 128-bit sizes and locations.
+  stack_top_pc_rel_0:
+  auipc sp, %pcrel_hi(KISTACK_TOP)
+  addi sp, sp, %pcrel_lo(stack_top_pc_rel_0)
   
-  # Load 0x8001FFF0 into Stack Pointer
-  ori a0, zero, 0x1FF
-  slli a0, a0, 4
-  lui sp, 0x4000F
-  slli sp, sp, 1
-  or sp, sp, a0
-  
+  # Zero all other registers
   mv ra, zero
   mv gp, zero
   mv tp, zero
@@ -63,12 +60,50 @@ my_entry_pt:
   mv t5, zero
   mv t6, zero
   
+  # Call into the C function
   call kmain
 
-loop:
+clear_and_loop:
+  mv sp, zero
+  mv ra, zero
+  mv gp, zero
+  mv tp, zero
+  mv t0, zero
+  mv t1, zero
+  mv t2, zero
+  mv fp, zero
+  mv s1, zero
+  mv a0, zero
+  mv a1, zero
+  mv a2, zero
+  mv a3, zero
+  mv a4, zero
+  mv a5, zero
+  mv a6, zero
+  mv a7, zero
+  mv s2, zero
+  mv s3, zero
+  mv s4, zero
+  mv s5, zero
+  mv s6, zero
+  mv s7, zero
+  mv s8, zero
+  mv s9, zero
+  mv s10, zero
+  mv s11, zero
+  mv t3, zero
+  mv t4, zero
+  mv t5, zero
+  mv t6, zero
+  loop:
   wfi
   j loop
 
+# Primative multiplication of registers s1 with s2. It
+# stores the result in s3 and returns.  It assumes
+# positive numbers in both s1 and s2.  It also does not
+# handle overflow.  It does not change the values of s1
+# or s2.  In addition to s3, it clobbers t0.
 mul:
   mv s3, zero
   mv t0, zero
@@ -80,4 +115,6 @@ mul:
   mul_end:
   ret
 
-j loop
+# This should not be reachable, but just in case,
+# jump back into the infinite loop.
+j clear_and_loop
--- src/progs/init/init.h
+++
@@ -1,1 +0,0 @@
-signed int init_main(unsigned int argc, char* argv[], char* envp[]);

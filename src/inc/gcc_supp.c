26cf92635458e74b6d519156f6b122d6297122b5
Michael T. Kloos <michael@michaelkloos.com>
2022-03-19 22:25:27 -0400

    Port my "ext_m_supplement.c" from my patch to the Linux kernel back to this project.  Here it is called "gcc_supp.c".
    I have ported the preprocessor directives as well as the coding
    style from the Linux kernel to this project.  Otherwise, it is the
    same code.
    
    This version should be more inline with correct and portable C, as
    well as includes implementaions for the __yxxxti3() series of
    integer arithmetic routines.  This should allow this code to work
    on a possible future 64-bit port of this project.

--- src/inc/gcc_supp.c
+++ src/inc/gcc_supp.c
@@ -1,43 +1,73 @@
-#include "types.h"
+/*
+ * Copyright (C) 2022 Michael T. Kloos <michael@michaelkloos.com>
+ */
 
 /*
-So my problem was not, in fact, a compiler bug.  It was incorrect 
-documentation.  The GNU manual page here: 
-https://gcc.gnu.org/onlinedocs/gccint/Integer-library-routines.html 
-describes these functions in terms of signed and unsigned; int, long, 
-and long long.  However, the RISC-V port of the GCC complier does 
-not follow that standard.  I'm not sure if the RISC-V port is unique 
-in the regard or if that page is simply wrong, possibly being 
-architecture specific without saying it.  
+ * The GNU manual page here:
+ * https://gcc.gnu.org/onlinedocs/gccint/Integer-library-routines.html
+ * describes these functions in terms of signed and unsigned, int and long.
+ * However, these prototypes are wrong when the size of int and long are
+ * considered per the RISC-V ABI specification.  The RISC-V port of the GCC
+ * complier does not follow the standard of those prototypes.  This is
+ * discussed in this thread:
+ * https://github.com/riscv-collab/riscv-gcc/issues/324
+ *
+ * On the RISC-V Architecture:
+ * - __xyyysi3 always refers to  32-bit integers.
+ * - __xyyydi3 always refers to  64-bit integers.
+ * - __xyyyti3 always refers to 128-bit integers. (Only implemented for rv64)
+ *
+ * Per the RISC-V ABI specification, the C base types are:
+ * - int:       32-bits wide.
+ * - long:      XLEN-bits wide.  It matches the register width.
+ * - long long: 64-bits wide.
+ *
+ * Therefore, the correct RISC-V function prototypes are:
+ * - signed int __mulsi3(signed int a, signed int b);
+ * - signed long long __muldi3(signed long long a, signed long long b);
+ * - signed __int128 __multi3(signed __int128 a, signed __int128 b);
+ *
+ * - signed int __divsi3(signed int a, signed int b);
+ * - signed long long __divdi3(signed long long a, signed long long b);
+ * - signed __int128 __divti3(signed __int128 a, signed __int128 b);
+ *
+ * - unsigned int __udivsi3(unsigned int a, unsigned int b);
+ * - unsigned long long __udivdi3(unsigned long long a, unsigned long long b);
+ * - unsigned __int128 __udivti3(unsigned __int128 a, unsigned __int128 b);
+ *
+ * - signed int __modsi3(signed int a, signed int b);
+ * - signed long long __moddi3(signed long long a, signed long long b);
+ * - signed __int128 __modti3(signed __int128 a, signed __int128 b);
+ *
+ * - unsigned int __umodsi3(unsigned int a, unsigned int b);
+ * - unsigned long long __umoddi3(unsigned long long a, unsigned long long b);
+ * - unsigned __int128 __umodti3(unsigned __int128 a, unsigned __int128 b);
+ */
 
-Per the RISC-V type sizes, listed in the RISC-V Calling Convention 
-specifications:
-- int:       32-bits wide.
-- long:      XLEN-bits wide.  It matches the register width.
-- long long: 64-bits wide.
+#ifndef __riscv_mul
 
-However the RISC-V GCC compiler considers functions to be like this: 
-- __***si*(): 32-bits
-- __***di*(): 64-bits
-- __***ti*(): 128-bits (As far as I know, this is not implemented 
-                        on RISC-V.  At least not on rv32)
+#if __riscv_xlen > 64
+#error "Unsupported XLEN"
+#endif
 
-Thank you for the help: 
-https://github.com/riscv-collab/riscv-gcc/issues/324
-*/
+#define UINT_MSB (((unsigned       int)1) <<  31)
+#define ULL_MSB  (((unsigned long long)1) <<  63)
+#if __riscv_xlen >= 64
+#define U128_MSB (((unsigned  __int128)1) << 127)
+#endif
 
-sint32_t __mulsi3(sint32_t a, sint32_t b) {
-	uint32_t ua = a;
-	uint32_t ub = b;
-	if (a < 0) {
-		ua = ~ua + 1;
-	}
-	if (b < 0) {
-		ub = ~ub + 1;
-	}
-	
-	uint32_t j = 0;
-	for (uint32_t i = 0; i < sizeof(sint32_t) * 8; i++) {
+signed int __mulsi3(signed int a, signed int b) {
+	unsigned int ua;
+	unsigned int ub;
+	unsigned int j;
+	unsigned int i;
+	signed int r;
+
+	ua = a;
+	ub = b;
+
+	j = 0;
+	for (i = 0; i < sizeof(signed int) * 8; i++) {
 		if (!ua || !ub) {
 			break;
 		}
@@ -47,29 +77,94 @@ sint32_t __mulsi3(sint32_t a, sint32_t b) {
 		ua >>= 1;
 		ub <<= 1;
 	}
-	
-	sint32_t r = j;
-	a ^= b;
-	if (a < 0) {
-		return -r;
-	}
+
+	r = j;
+
 	return r;
 }
 
-sint32_t __modsi3(sint32_t a, sint32_t b) {
-	uint32_t ua = a;
-	uint32_t ub = b;
+signed long long __muldi3(signed long long a, signed long long b) {
+	unsigned long long ua;
+	unsigned long long ub;
+	unsigned long long j;
+	unsigned int i;
+	signed long long r;
+
+	ua = a;
+	ub = b;
+
+	j = 0;
+	for (i = 0; i < sizeof(signed long long) * 8; i++) {
+		if (!ua || !ub) {
+			break;
+		}
+		if (ua & 0x1) {
+			j += ub;
+		}
+		ua >>= 1;
+		ub <<= 1;
+	}
+
+	r = j;
+
+	return r;
+}
+
+#if __riscv_xlen >= 64
+signed __int128 __multi3(signed __int128 a, signed __int128 b) {
+	unsigned __int128 ua;
+	unsigned __int128 ub;
+	unsigned __int128 j;
+	unsigned int i;
+	signed __int128 r;
+
+	ua = a;
+	ub = b;
+
+	j = 0;
+	for (i = 0; i < sizeof(signed __int128) * 8; i++) {
+		if (!ua || !ub) {
+			break;
+		}
+		if (ua & 0x1) {
+			j += ub;
+		}
+		ua >>= 1;
+		ub <<= 1;
+	}
+
+	r = j;
+
+	return r;
+}
+#endif
+
+signed int __divsi3(signed int a, signed int b) {
+	unsigned int ua;
+	unsigned int ub;
+	unsigned int j;
+	unsigned int i;
+	signed int r;
+
+	if (b == 0) {
+		return (signed int)(-1);
+	}
+
+	ua = a;
+	ub = b;
 	if (a < 0) {
-		ua = ~ua + 1;
+		ua = -a;
 	}
 	if (b < 0) {
-		ub = ~ub + 1;
+		ub = -b;
 	}
-	
-	uint32_t i = 0;
+
+	j = 0;
+	i = 0;
 	while (ua >= ub) {
-		if ((sint32_t)ub < 0) {
+		if (ub & UINT_MSB) {
 			ua -= ub;
+			j |= 1u << i;
 			break;
 		}
 		ub <<= 1;
@@ -80,72 +175,44 @@ sint32_t __modsi3(sint32_t a, sint32_t b) {
 		ub >>= 1;
 		if (ua >= ub) {
 			ua -= ub;
+			j |= 1u << i;
 		}
 	}
-	
-	sint32_t r = ua;
-	if (a < 0) {
-		return -r;
+
+	r = j;
+	if ((a < 0) != ((b < 0))) {
+		r = -r;
 	}
+
 	return r;
 }
 
-uint32_t __umodsi3(uint32_t a, uint32_t b) {
-	uint32_t i = 0;
-	while (a >= b) {
-		if ((sint32_t)b < 0) {
-			a -= b;
-			break;
-		}
-		b <<= 1;
-		i++;
-	}
-	while (i > 0) {
-		i--;
-		b >>= 1;
-		if (a >= b) {
-			a -= b;
-		}
-	}
-	return a;
-}
+signed long long __divdi3(signed long long a, signed long long b) {
+	unsigned long long ua;
+	unsigned long long ub;
+	unsigned long long j;
+	unsigned int i;
+	signed long long r;
 
-uint64_t __umoddi3(uint64_t a, uint64_t b) {
-	uint64_t i = 0;
-	while (a >= b) {
-		if ((sint64_t)b < 0) {
-			a -= b;
-			break;
-		}
-		b <<= 1;
-		i++;
+	if (b == 0) {
+		return (signed long long)(-1);
 	}
-	while (i > 0) {
-		i--;
-		b >>= 1;
-		if (a >= b) {
-			a -= b;
-		}
-	}
-	return a;
-}
 
-sint32_t __divsi3(sint32_t a, sint32_t b) {
-	uint32_t ua = a;
-	uint32_t ub = b;
+	ua = a;
+	ub = b;
 	if (a < 0) {
-		ua = ~ua + 1;
+		ua = -a;
 	}
 	if (b < 0) {
-		ub = ~ub + 1;
+		ub = -b;
 	}
-	
-	uint32_t i = 0;
-	uint32_t j = 0;
+
+	j = 0;
+	i = 0;
 	while (ua >= ub) {
-		if ((sint32_t)ub < 0) {
+		if (ub & ULL_MSB) {
 			ua -= ub;
-			j |= 1 << i;
+			j |= 1ull << i;
 			break;
 		}
 		ub <<= 1;
@@ -156,25 +223,82 @@ sint32_t __divsi3(sint32_t a, sint32_t b) {
 		ub >>= 1;
 		if (ua >= ub) {
 			ua -= ub;
-			j |= 1 << i;
+			j |= 1ull << i;
 		}
 	}
-	
-	sint32_t r = j;
-	a ^= b;
-	if (a < 0) {
-		return -r;
+
+	r = j;
+	if ((a < 0) != ((b < 0))) {
+		r = -r;
 	}
+
 	return r;
 }
 
-uint32_t __udivsi3(uint32_t a, uint32_t b) {
-	uint32_t i = 0;
-	uint32_t j = 0;
+#if __riscv_xlen >= 64
+signed __int128 __divti3(signed __int128 a, signed __int128 b) {
+	unsigned __int128 ua;
+	unsigned __int128 ub;
+	unsigned __int128 j;
+	unsigned int i;
+	signed __int128 r;
+
+	if (b == 0) {
+		return (signed __int128)(-1);
+	}
+
+	ua = a;
+	ub = b;
+	if (a < 0) {
+		ua = -a;
+	}
+	if (b < 0) {
+		ub = -b;
+	}
+
+	j = 0;
+	i = 0;
+	while (ua >= ub) {
+		if (ub & U128_MSB) {
+			ua -= ub;
+			j |= ((unsigned __int128)1) << i;
+			break;
+		}
+		ub <<= 1;
+		i++;
+	}
+	while (i > 0) {
+		i--;
+		ub >>= 1;
+		if (ua >= ub) {
+			ua -= ub;
+			j |= ((unsigned __int128)1) << i;
+		}
+	}
+
+	r = j;
+	if ((a < 0) != ((b < 0))) {
+		r = -r;
+	}
+
+	return r;
+}
+#endif
+
+unsigned int __udivsi3(unsigned int a, unsigned int b) {
+	unsigned int j;
+	unsigned int i;
+
+	if (b == 0) {
+		return (signed int)(-1);
+	}
+
+	j = 0;
+	i = 0;
 	while (a >= b) {
-		if ((sint32_t)b < 0) {
+		if (b & UINT_MSB) {
 			a -= b;
-			j |= 1 << i;
+			j |= 1u << i;
 			break;
 		}
 		b <<= 1;
@@ -185,61 +309,25 @@ uint32_t __udivsi3(uint32_t a, uint32_t b) {
 		b >>= 1;
 		if (a >= b) {
 			a -= b;
-			j |= 1 << i;
+			j |= 1u << i;
 		}
 	}
+
 	return j;
 }
 
-uint64_t __udivdi3(uint64_t a, uint64_t b) {
-	uint64_t i = 0;
-	uint64_t j = 0;
-	while (a >= b) {
-		if ((sint64_t)b < 0) {
-			a -= b;
-			j |= 1ul << i;
-			break;
-		}
-		b <<= 1;
-		i++;
-	}
-	while (i > 0) {
-		i--;
-		b >>= 1;
-		if (a >= b) {
-			a -= b;
-			j |= 1ul << i;
-		}
-	}
-	return j;
-}
+unsigned long long __udivdi3(unsigned long long a, unsigned long long b) {
+	unsigned long long j;
+	unsigned long long i;
 
-/*
-unsigned long long __umodti3(unsigned long long a, unsigned long long b) {
-	unsigned long i = 0;
-	while (a >= b) {
-		if ((signed long long)b < 0) {
-			a -= b;
-			break;
-		}
-		b <<= 1;
-		i++;
+	if (b == 0) {
+		return (signed long long)(-1);
 	}
-	while (i > 0) {
-		i--;
-		b >>= 1;
-		if (a >= b) {
-			a -= b;
-		}
-	}
-	return a;
-}
 
-unsigned long long __udivti3(unsigned long long a, unsigned long long b) {
-	unsigned long i = 0;
-	unsigned long long j = 0;
+	j = 0;
+	i = 0;
 	while (a >= b) {
-		if ((signed long long)b < 0) {
+		if (b & ULL_MSB) {
 			a -= b;
 			j |= 1ull << i;
 			break;
@@ -255,6 +343,258 @@ unsigned long long __udivti3(unsigned long long a, unsigned long long b) {
 			j |= 1ull << i;
 		}
 	}
+
 	return j;
 }
-*/
+
+#if __riscv_xlen >= 64
+unsigned __int128 __udivti3(unsigned __int128 a, unsigned __int128 b) {
+	unsigned __int128 j;
+	unsigned __int128 i;
+
+	if (b == 0) {
+		return (signed __int128)(-1);
+	}
+
+	j = 0;
+	i = 0;
+	while (a >= b) {
+		if (b & U128_MSB) {
+			a -= b;
+			j |= ((unsigned __int128)1) << i;
+			break;
+		}
+		b <<= 1;
+		i++;
+	}
+	while (i > 0) {
+		i--;
+		b >>= 1;
+		if (a >= b) {
+			a -= b;
+			j |= ((unsigned __int128)1) << i;
+		}
+	}
+
+	return j;
+}
+#endif
+
+signed int __modsi3(signed int a, signed int b) {
+	unsigned int ua;
+	unsigned int ub;
+	unsigned int i;
+	signed int r;
+
+	if (b == 0) {
+		return a;
+	}
+
+	ua = a;
+	ub = b;
+	if (a < 0) {
+		ua = -a;
+	}
+	if (b < 0) {
+		ub = -b;
+	}
+
+	i = 0;
+	while (ua >= ub) {
+		if (ub & UINT_MSB) {
+			ua -= ub;
+			break;
+		}
+		ub <<= 1;
+		i++;
+	}
+	while (i > 0) {
+		i--;
+		ub >>= 1;
+		if (ua >= ub) {
+			ua -= ub;
+		}
+	}
+
+	r = ua;
+	if (a < 0) {
+		r = -r;
+	}
+
+	return r;
+}
+
+signed long long __moddi3(signed long long a, signed long long b) {
+	unsigned long long ua;
+	unsigned long long ub;
+	unsigned int i;
+	signed long long r;
+
+	if (b == 0) {
+		return a;
+	}
+
+	ua = a;
+	ub = b;
+	if (a < 0) {
+		ua = -a;
+	}
+	if (b < 0) {
+		ub = -b;
+	}
+
+	i = 0;
+	while (ua >= ub) {
+		if (ub & ULL_MSB) {
+			ua -= ub;
+			break;
+		}
+		ub <<= 1;
+		i++;
+	}
+	while (i > 0) {
+		i--;
+		ub >>= 1;
+		if (ua >= ub) {
+			ua -= ub;
+		}
+	}
+
+	r = ua;
+	if (a < 0) {
+		r = -r;
+	}
+
+	return r;
+}
+
+#if __riscv_xlen >= 64
+signed __int128 __modti3(signed __int128 a, signed __int128 b) {
+	unsigned __int128 ua;
+	unsigned __int128 ub;
+	unsigned int i;
+	signed __int128 r;
+
+	if (b == 0) {
+		return a;
+	}
+
+	ua = a;
+	ub = b;
+	if (a < 0) {
+		ua = -a;
+	}
+	if (b < 0) {
+		ub = -b;
+	}
+
+	i = 0;
+	while (ua >= ub) {
+		if (ub & U128_MSB) {
+			ua -= ub;
+			break;
+		}
+		ub <<= 1;
+		i++;
+	}
+	while (i > 0) {
+		i--;
+		ub >>= 1;
+		if (ua >= ub) {
+			ua -= ub;
+		}
+	}
+
+	r = ua;
+	if (a < 0) {
+		r = -r;
+	}
+
+	return r;
+}
+#endif
+
+unsigned int __umodsi3(unsigned int a, unsigned int b) {
+	unsigned int i;
+
+	if (b == 0) {
+		return a;
+	}
+
+	i = 0;
+	while (a >= b) {
+		if (b & UINT_MSB) {
+			a -= b;
+			break;
+		}
+		b <<= 1;
+		i++;
+	}
+	while (i > 0) {
+		i--;
+		b >>= 1;
+		if (a >= b) {
+			a -= b;
+		}
+	}
+
+	return a;
+}
+
+unsigned long long __umoddi3(unsigned long long a, unsigned long long b) {
+	unsigned long long i;
+
+	if (b == 0) {
+		return a;
+	}
+
+	i = 0;
+	while (a >= b) {
+		if (b & ULL_MSB) {
+			a -= b;
+			break;
+		}
+		b <<= 1;
+		i++;
+	}
+	while (i > 0) {
+		i--;
+		b >>= 1;
+		if (a >= b) {
+			a -= b;
+		}
+	}
+
+	return a;
+}
+
+#if __riscv_xlen >= 64
+unsigned __int128 __umodti3(unsigned __int128 a, unsigned __int128 b) {
+	unsigned __int128 i;
+
+	if (b == 0) {
+		return a;
+	}
+
+	i = 0;
+	while (a >= b) {
+		if (b & U128_MSB) {
+			a -= b;
+			break;
+		}
+		b <<= 1;
+		i++;
+	}
+	while (i > 0) {
+		i--;
+		b >>= 1;
+		if (a >= b) {
+			a -= b;
+		}
+	}
+
+	return a;
+}
+#endif
+
+#endif

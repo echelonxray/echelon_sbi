echelonxray/echelon_sbi

commit 9647e558785e33496e3725bf548fd3d9f2e22924

Fixes for the sstc isa extension.
This allows the SBI time extension, to sent timers when the sstc
extension is in use.  It is possible for the supervisor to use
the SBI calls to set the timers instead of using
stimecmp(/stimecmph) directly when the sstc isa extension is in
use.

src/sbi_commands/time.c
@@ -3,46 +3,41 @@
 #include <inc/csr.h>
 
 extern __thread uintRL_t mhartid;
+extern __thread uint8_t hart_using_ext_sstc;
 
 struct sbiret sbi_set_timer(uint64_t stime_value) {
-	CSRI_BITCLR(CSR_MIE, 0x80);
-	CSRI_BITCLR(CSR_MIP, 0x20);
-	
-#ifdef MM_CUSTOM_EMU
-	uint32_t mtime;
-	uint32_t mtimeh;
-	mtimeh = (uint32_t)((stime_value >> 32) & 0xFFFFFFFF);
-	mtime  = (uint32_t)((stime_value >>  0) & 0xFFFFFFFF);
-	
-	volatile uint32_t* mtimecmp = (void*)(((uintRL_t)CLINT_BASE) + CLINT_MTIMECMPS);
-	volatile uint32_t* mtimecmphi = (void*)(((uintRL_t)CLINT_BASE) + CLINT_MTIMECMPS + 0x4);
-	mtimecmphi[mhartid * 2] = (sint32_t)-1;
-	mtimecmp[mhartid * 2]   = mtime;
-	mtimecmphi[mhartid * 2] = mtimeh;
-#endif
-
-#ifdef MM_QEMU_VIRT
 
 #if   __riscv_xlen == 64
-	volatile uint64_t* mtimecmp = (void*)(((uintRL_t)CLINT_BASE) + CLINT_MTIMECMPS);
-	mtimecmp[mhartid] = stime_value;
+	if (hart_using_ext_sstc) {
+		CSRI_WRITE(CSR_STIMECMP, stime_value);
+	} else {
+		CSRI_BITCLR(CSR_MIE, 0x80);
+		CSRI_BITCLR(CSR_MIP, 0x20);
+
+		volatile uint64_t* mtimecmp = (void*)(((uintRL_t)CLINT_BASE) + CLINT_MTIMECMPS);
+		mtimecmp[mhartid] = stime_value;
+	}
 #elif __riscv_xlen == 32
-	uint32_t mtime;
-	uint32_t mtimeh;
-	mtimeh = (uint32_t)((stime_value >> 32) & 0xFFFFFFFF);
-	mtime  = (uint32_t)((stime_value >>  0) & 0xFFFFFFFF);
-	
-	volatile uint32_t* mtimecmp   = (void*)(((uintRL_t)CLINT_BASE) + CLINT_MTIMECMPS);
-	volatile uint32_t* mtimecmphi = (void*)(((uintRL_t)CLINT_BASE) + CLINT_MTIMECMPS + 0x4);
-	mtimecmphi[mhartid * 2] = (sint32_t)-1;
-	mtimecmp[mhartid * 2] = mtime;
-	mtimecmphi[mhartid * 2] = mtimeh;
+	uint32_t xtime;
+	uint32_t xtimeh;
+	xtimeh = (uint32_t)((stime_value >> 32) & 0xFFFFFFFF);
+	xtime  = (uint32_t)((stime_value >>  0) & 0xFFFFFFFF);
+
+	if (hart_using_ext_sstc) {
+		CSRI_WRITE(CSR_STIMECMPH, (sint32_t)-1);
+		CSRI_WRITE(CSR_STIMECMP, xtime);
+		CSRI_WRITE(CSR_STIMECMPH, xtimeh);
+	} else {
+		volatile uint32_t* mtimecmp   = (void*)(((uintRL_t)CLINT_BASE) + CLINT_MTIMECMPS);
+		volatile uint32_t* mtimecmphi = (void*)(((uintRL_t)CLINT_BASE) + CLINT_MTIMECMPS + 0x4);
+		mtimecmphi[mhartid * 2] = (sint32_t)-1;
+		mtimecmp[mhartid * 2] = xtime;
+		mtimecmphi[mhartid * 2] = xtimeh;
+
+		CSRI_BITSET(CSR_MIE, 0x80);
+	}
 #endif
 
-#endif
-	
-	CSRI_BITSET(CSR_MIE, 0x80);
-	
 	struct sbiret retval;
 	retval.value = 0;
 	retval.error = SBI_SUCCESS;

src/inc/csr.h
@@ -12,6 +12,10 @@
 #define CSR_SCAUSE         0x142
 #define CSR_STVAL          0x143
 #define CSR_SIP            0x144
+
+#define CSR_STIMECMP       0x14D
+#define CSR_STIMECMPH      0x15D
+
 #define CSR_SATP           0x180
 
 #define CSR_MVENDORID      0xF11

src/interrupts/context_switch.c
@@ -22,6 +22,7 @@ extern uintRL_t kernel_load_to_point;
 
 extern __thread uintRL_t mhartid;
 extern __thread uintRL_t hart_has_menvcfg;
+__thread uint8_t hart_using_ext_sstc = 0;
 
 void print_reg_state(volatile CPU_Context* cpu_context) {
 	printm("----Reg State----\n");
@@ -92,15 +93,26 @@ void interrupt_c_handler(volatile CPU_Context* cpu_context, uintRL_t cause_value
 			//   behind bitflags in MENVCFG and MCOUNTEREN.  If these are not set by firmware, supervisor
 			//   attempts to access stimecmp (or stimecmph if rv32) will throw an Illegal Instruction
 			//   Exception.
-			CSRI_BITSET(CSR_MCOUNTEREN, 0x2);
 			if (hart_has_menvcfg) {
 #if   __riscv_xlen == 128
 				#error "Not Implemented"
 #elif __riscv_xlen == 64
-				CSRI_BITSET(CSR_MENVCFG, 1 << 63);
+				CSRI_BITSET(CSR_MENVCFG, 1ul << 63);
+				uintRL_t csr_menucfg = CSRI_BITCLR(CSR_MENVCFG, 0);
+				if (csr_menucfg & (1ul << 63)) {
+					hart_using_ext_sstc = 1;
+				}
 #elif __riscv_xlen == 32
-				CSRI_BITSET(CSR_MENVCFGH, 1 << 31);
+				CSRI_BITSET(CSR_MENVCFGH, 1ul << 31);
+				uintRL_t csr_menucfgh = CSRI_BITCLR(CSR_MENVCFGH, 0);
+				if (csr_menucfgh & (1ul << 31)) {
+					hart_using_ext_sstc = 1;
+				}
 #endif
+			}
+			if (hart_using_ext_sstc) {
+				printm("\tUsing ISA Extenstion: SSTC\n");
+				CSRI_BITSET(CSR_MCOUNTEREN, 0x2);
 			}
 
 			// QEMU Fixes - Zicboz, Zicbom, Zicbop
@@ -117,9 +129,9 @@ void interrupt_c_handler(volatile CPU_Context* cpu_context, uintRL_t cause_value
 			//   Instruction Exception from S-mode will be generated.  An alternaive fix is to build the Kernel
 			//   without Zicbom support.
 			if (hart_has_menvcfg) {
-				CSRI_BITSET(CSR_MENVCFG, 3 << 4);
-				CSRI_BITSET(CSR_MENVCFG, 1 << 6);
-				CSRI_BITSET(CSR_MENVCFG, 1 << 7);
+				CSRI_BITSET(CSR_MENVCFG, 3ul << 4);
+				CSRI_BITSET(CSR_MENVCFG, 1ul << 6);
+				CSRI_BITSET(CSR_MENVCFG, 1ul << 7);
 			}
 #endif
 
@@ -129,11 +141,12 @@ void interrupt_c_handler(volatile CPU_Context* cpu_context, uintRL_t cause_value
 			// Delegate S-Mode Timer Interrupt to S-Mode
 			// Delegate S-Mode External Interrupt to S-Mode
 			CSRI_WRITE(CSR_MIDELEG, 0x0222);
+			//CSRI_WRITE(CSR_MIDELEG, 0x0000);
 			
-			// Enable S-Mode Timer Interrupt
 			// Enable S-Mode Software Interrupt
 			// Enable M-Mode Software Interrupt
-			CSRI_WRITE( CSR_MIE, 0x02A);
+			// Enable S-Mode Timer Interrupt
+			CSRI_WRITE(CSR_MIE, 0x02A);
 			// Clear all pending Interrupts
 			CSRI_BITCLR(CSR_MIP, 0xFFF);
 			
@@ -200,10 +213,13 @@ void interrupt_c_handler(volatile CPU_Context* cpu_context, uintRL_t cause_value
 		idle_loop();
 	} else if (cause_value == 7) {
 		// M-Mode Timer Interrupt
-		
+		//printm("ESBI: M-Mode Timer Interrupt.\n");
+
 		// Bounce this into S-Mode
-		CSRI_BITCLR(CSR_MIE, 0x80);
-		CSRI_BITSET(CSR_MIP, 0x20);
+		if (!hart_using_ext_sstc) {
+			CSRI_BITCLR(CSR_MIE, 0x80);
+			CSRI_BITSET(CSR_MIP, 0x20);
+		}
 	} else if (cause_value == 11) {
 		// M-Mode External Interrupt
 		

commit 577088dd97a19a0eb57196a94351bbd615444653

Fix No-SSTC timer setting.
This was broken in commit:
9647e558785e33496e3725bf548fd3d9f2e22924
Fixes for the sstc isa extension.

src/sbi_commands/time.c
@@ -16,6 +16,8 @@ struct sbiret sbi_set_timer(uint64_t stime_value) {
 
 		volatile uint64_t* mtimecmp = (void*)(((uintRL_t)CLINT_BASE) + CLINT_MTIMECMPS);
 		mtimecmp[mhartid] = stime_value;
+
+		CSR_BITSET(CSR_MIE, 0x80);
 	}
 #elif __riscv_xlen == 32
 	uint32_t xtime;
@@ -28,6 +30,9 @@ struct sbiret sbi_set_timer(uint64_t stime_value) {
 		CSR_WRITE(CSR_STIMECMP, xtime);
 		CSR_WRITE(CSR_STIMECMPH, xtimeh);
 	} else {
+		CSR_BITCLR(CSR_MIE, 0x80);
+		CSR_BITCLR(CSR_MIP, 0x20);
+
 		volatile uint32_t* mtimecmp   = (void*)(((uintRL_t)CLINT_BASE) + CLINT_MTIMECMPS);
 		volatile uint32_t* mtimecmphi = (void*)(((uintRL_t)CLINT_BASE) + CLINT_MTIMECMPS + 0x4);
 		mtimecmphi[mhartid * 2] = (sint32_t)-1;